90d8ce5a06ee604afd6c3666f245e118fc327beb
Pratik sharma <sharma.pratik2016@gmail.com>
2023-06-29 02:52:50 +0530

    initial commit

---
+++ micro-service/routes/order.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send({ message: "get all the orders" });
+});
+
+router.post("/", (req, res) => {
+  res.send({ message: "order ", id: "llkml" });
+});
+
+module.exports = router;
